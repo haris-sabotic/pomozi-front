26272b8fb2c407bb9773646a67e4c7157327bece
haris-sabotic <haris.sinfo16@gmail.com>
2024-01-27 21:45:45 +0100

    Initial commit

---
+++ org_data.js
@@ -0,0 +1,14 @@
+const ORGANIZATIONS = {
+    "crveni_krst": {
+        name: "Crveni krst",
+        donated_to: "CRVENOM KRSTU"
+    },
+    "budi_human": {
+        name: "Budi human",
+        donated_to: "ORGANIZACIJI BUDI HUMAN"
+    },
+    "humanost": {
+        name: "Humanost",
+        donated_to: "ORGANIZACIJI HUMANOST"
+    },
+};
